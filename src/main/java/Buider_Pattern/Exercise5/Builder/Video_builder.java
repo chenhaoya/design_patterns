c405221f2ab91e7d78600a276f7b07d7a16ce150
chenhaoya <1627785181@qq.com>
2021-09-28 16:10:32 +0800

    "7"

---
+++ src/main/java/Buider_Pattern/Exercise5/Builder/Video_builder.java
@@ -0,0 +1,19 @@
+/**
+ * 作者：陈浩
+ * 时间：2021/9/28 16:05
+ * 说明：
+ */
+package Buider_Pattern.Exercise5.Builder;
+
+import Buider_Pattern.Exercise5.Product.Video_play;
+
+public abstract class Video_builder {
+     protected Video_play video_play = new Video_play();
+     public abstract void buildDisplayMenu();
+     public abstract void buildPlaylist();
+     public abstract void buildmainWindow();
+     public abstract void buildcontrolStrip();
+     public Video_play createvideoPlay(){
+         return video_play;
+     }
+}
---
+++ src/main/java/Buider_Pattern/Exercise5/Client.java
@@ -0,0 +1,9 @@
+/**
+ * 作者：陈浩
+ * 时间：2021/9/28 15:52
+ * 说明：
+ */
+package Buider_Pattern.Exercise5;
+
+public class Client {
+}
