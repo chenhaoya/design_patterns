833fe3708aadad6dfaed391fe290fb7823aa0ce6
chenhaoya <1627785181@qq.com>
2021-11-08 16:18:31 +0800

    "0"

---
+++ src/main/java/Structural_Pattern/Proxy_Pattern/Practice6/Client.java
@@ -0,0 +1,16 @@
+/**
+ * 开发团队：复仇者联盟
+ * 开发团队领导人：陈浩
+ * 开发人员姓名：陈浩
+ * 学号/工号：2019112102
+ * 个人/公司邮箱：ch111222@qq.com
+ * 时间：2021/11/8 15:22
+ * 开发名称：Client
+ * 开发工具：IntelliJ IDEA
+ * 当前用户：CH
+ * 描述：
+ */
+package Structural_Pattern.Proxy_Pattern.Practice6;
+
+public class Client {
+}
