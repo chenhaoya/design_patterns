bbba7fe6bd3f8d57fa71b5b77625fda646657cac
chenhaoya <1627785181@qq.com>
2021-11-09 17:04:10 +0800

    "0"

---
+++ src/main/java/Structural_Pattern/Chain_of_Responsibility_Pattern/Practice4/ConcreteHandler/BanZhang.java
@@ -0,0 +1,37 @@
+/**
+ * 开发团队：复仇者联盟
+ * 开发团队领导人：陈浩
+ * 开发人员姓名：陈浩
+ * 学号/工号：2019112102
+ * 个人/公司邮箱：ch111222@qq.com
+ * 时间：2021/11/9 16:11
+ * 开发名称：BanZhang
+ * 开发工具：IntelliJ IDEA
+ * 当前用户：CH
+ * 描述：
+ */
+package Structural_Pattern.Chain_of_Responsibility_Pattern.Practice4.ConcreteHandler;
+
+import Structural_Pattern.Chain_of_Responsibility_Pattern.Practice4.Handler.Approver;
+import Structural_Pattern.Chain_of_Responsibility_Pattern.Practice4.War_Request;
+
+public class BanZhang extends Approver {
+    public BanZhang(String name_) {
+        super(name_);
+    }
+
+    @Override
+    public void setSuccessor(Approver successor) {
+        super.setSuccessor(successor);
+    }
+
+    @Override
+    public void processResponse(War_Request request) {
+        if (request.getNumber_()<10){
+            System.out.println(this.name_+"递交的任务书");
+            System.out.println("指挥；" + this.name_ + "规模：" + request.getNumber_()+"作战名："+request.getWar_name());
+        }else{
+            this.approver.processResponse(request);
+        }
+    }
+}
---
+++ src/main/java/Structural_Pattern/Chain_of_Responsibility_Pattern/Practice4/Handler/Approver.java
@@ -0,0 +1,29 @@
+/**
+ * 开发团队：复仇者联盟
+ * 开发团队领导人：陈浩
+ * 开发人员姓名：陈浩
+ * 学号/工号：2019112102
+ * 个人/公司邮箱：ch111222@qq.com
+ * 时间：2021/11/9 16:08
+ * 开发名称：Approver
+ * 开发工具：IntelliJ IDEA
+ * 当前用户：CH
+ * 描述：
+ */
+package Structural_Pattern.Chain_of_Responsibility_Pattern.Practice4.Handler;
+
+import Structural_Pattern.Chain_of_Responsibility_Pattern.Practice4.War_Request;
+
+public abstract class Approver {
+    protected Approver approver;
+    protected String name_;
+
+    public Approver(String name_) {
+        this.name_ = name_;
+    }
+
+    public void setSuccessor(Approver successor){
+        this.approver = successor;
+    }
+    public abstract void processResponse(War_Request request);
+}
